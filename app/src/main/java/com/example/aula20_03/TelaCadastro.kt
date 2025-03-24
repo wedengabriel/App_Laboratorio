5957d106e684548d36dd4526b20683d04bbafe70
weden <weden.gabrielmt@gmail.com>
2025-03-24 19:59:47 -0400

    Update APP

--- app/src/main/java/com/example/aula20_03/TelaCadastro.kt
+++ app/src/main/java/com/example/aula20_03/TelaCadastro.kt
@@ -1,7 +1,5 @@
 package com.example.aula20_03
 
-import android.annotation.SuppressLint
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -10,11 +8,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.firebase.auth.FirebaseAuth
+import android.content.Intent
 import android.widget.Toast
-
+import com.google.firebase.Firebase
 
 class TelaCadastro : AppCompatActivity() {
-    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -25,25 +23,28 @@ class TelaCadastro : AppCompatActivity() {
             insets
         }
 
+        val botaoVoltar = findViewById<Button>(R.id.voltar)
         val textFuncionario = findViewById<EditText>(R.id.Funcionario)
-        val textEmail = findViewById<EditText>(R.id.Email)
-        val textSenha = findViewById<Button>(R.id.Senha)
+        val textemail = findViewById<EditText>(R.id.email)
+        val textsenha = findViewById<EditText>(R.id.senha)
         val botaoCadastrarUsuario = findViewById<Button>(R.id.CadastrarUsuario)
         val auth = FirebaseAuth.getInstance()
 
-        botaoCadastrarUsuario.setOnClickListener{
+        botaoVoltar.setOnClickListener{
 
-            auth.createUserWithEmailAndPassword(textEmail.text.toString(), textSenha.text.toString())
+            val intent = Intent(this@TelaCadastro, MainActivity::class.java)
+            startActivity(intent)
+
+        }
+
+        botaoCadastrarUsuario.setOnClickListener{
+            auth.createUserWithEmailAndPassword(textemail.text.toString(), textsenha.text.toString())
                 .addOnSuccessListener {
                     Toast.makeText(this, "Cadastro realizado com sucesso", Toast.LENGTH_SHORT).show()
                 }
                 .addOnFailureListener(){
                     Toast.makeText(this, "Nãoo foi possível efetuar o cadastro", Toast.LENGTH_SHORT).show()
                 }
-
-            val intent = Intent(this@TelaCadastro, MainActivity::class.java)
-            startActivity(intent)
-
         }
     }
 }
